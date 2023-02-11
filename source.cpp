aa028aceb83bdd2b6d250cd6e8f4fb8e6b5ea852
DIA <mrbrused20@gmail.com>
2023-02-11 21:28:57 +0300

    2^x + 2^y

--- source.cpp
+++ source.cpp
@@ -1,11 +1,17 @@
 #include <iostream>
 using namespace std;
 int main(int argc, char* argv[]){
-	cout << "Input a number" << endl;
+	cout << "Input first number" << endl;
 	int k;
 	cin >> k;
 	unsigned int a = 0x01;
 	a = a << k;
-	cout << a << endl;
+	cout << "Input second number" << endl;
+	int n;
+	cin >> n;
+	unsigned int b = 0x01;
+	b = b << n;
+	int c = a|b;
+	cout << c;
 	return 0;
 }
